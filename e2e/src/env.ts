0097bc8dd4d67bf0af8f03e2980bc4794e8c2664
izzy <me@insrt.uk>
2026-01-20 11:40:40 +0000

    chore: move tasks around

--- e2e/src/env.ts
+++ e2e/src/env.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 const schema = z.object({
-  RESTIC_API_PORT: z.coerce.number().min(1000).default(3010),
-  YUCCA_API_PORT: z.coerce.number().min(1000).default(3000),
+  RESTIC_API_PORT: z.coerce.number().min(1000),
+  YUCCA_API_PORT: z.coerce.number().min(1000),
 
   JWT_SECRET: z.string().min(32),
 });
--- restic-api/src/env.ts
+++ restic-api/src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const schema = z.object({
-  RESTIC_API_PORT: z.coerce.number().min(1000).default(3010),
+  RESTIC_API_PORT: z.coerce.number().min(1000),
 
   JWT_SECRET: z.string().min(32),
 
