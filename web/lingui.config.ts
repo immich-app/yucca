09f8d16771f4cb66102b530c9964ae12e4da7127
izzy <me@insrt.uk>
2026-01-26 16:29:13 +0000

    fix: use valid language code

--- web/lingui.config.ts
+++ web/lingui.config.ts
@@ -1,7 +1,7 @@
 import { jstsExtractor, svelteExtractor } from 'svelte-i18n-lingui/extractor';
 
 export default {
-  locales: ['en', 'test'],
+  locales: ['en', 'ja'],
   sourceLocale: 'en',
   catalogs: [
     {
