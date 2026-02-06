cfb7d1eaa0544a4e5e0265dc00462a8cbb870d78
izzy <me@insrt.uk>
2026-02-06 16:00:29 +0000

    feat: API client for ui

--- packages/yucca-sdk/orchestration-api/src/orchestration-api.module.ts
+++ packages/yucca-sdk/orchestration-api/src/orchestration-api.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { OrchestrationApiController } from './orchestration-api.controller';
 import { OrchestrationApiService } from './orchestration-api.service';
 
 @Module({
+  controllers: [OrchestrationApiController],
   providers: [OrchestrationApiService],
   exports: [OrchestrationApiService],
 })
---
+++ packages/yucca-sdk/orchestration-ui/src/lib/fetch-client.ts
@@ -0,0 +1,21 @@
+/**
+ * yucca
+ * 1.0.0
+ * DO NOT MODIFY - This file has been generated using oazapfts.
+ * See https://www.npmjs.com/package/oazapfts
+ */
+import * as Oazapfts from "@oazapfts/runtime";
+import * as QS from "@oazapfts/runtime/query";
+export const defaults: Oazapfts.Defaults<Oazapfts.CustomHeaders> = {
+    headers: {},
+    baseUrl: "/"
+};
+const oazapfts = Oazapfts.runtime(defaults);
+export const servers = {
+    server1: "/"
+};
+export function test(opts?: Oazapfts.RequestOpts) {
+    return oazapfts.ok(oazapfts.fetchText("/", {
+        ...opts
+    }));
+}
