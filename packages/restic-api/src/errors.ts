c7f78f3735412688f6f05869ecffa7623afc97f9
Paul Makles <me@insrt.uk>
2026-02-18 15:21:51 +0000

    fix: chunked uploads fail checksum checks (#38)

--- packages/restic-api/src/errors.ts
+++ packages/restic-api/src/errors.ts
@@ -1,7 +1,13 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, InternalServerErrorException } from '@nestjs/common';
 
 export class S3Error extends InternalServerErrorException {
   constructor() {
     super('An error occurred with the storage server');
+  }
+}
+
+export class ChecksumMismatchError extends BadRequestException {
+  constructor() {
+    super('Content hash does not match blob name');
   }
 }
