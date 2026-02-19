7c063cfbb1bea35c3c43afae49975910b1da7517
izzy <me@insrt.uk>
2026-02-19 10:14:41 +0000

    feat: 'bytes stored' metric
    Signed-off-by: izzy <me@insrt.uk>

--- packages/restic-api/src/utils/meters.ts
+++ packages/restic-api/src/utils/meters.ts
@@ -2,10 +2,16 @@ import { Attributes, Counter } from '@opentelemetry/api';
 import { PassThrough, Readable } from 'node:stream';
 import { AuthDto } from 'src/dto/auth.dto';
 
-export function attachMeterToStream(stream: Readable, meterBytes: Counter, context: Attributes) {
+export function attachMeterToStream(
+  stream: Readable,
+  meterBytes: Counter,
+  callbackBytes: (bytes: number) => void,
+  context: Attributes,
+) {
   const passthrough = new PassThrough({
     transform(chunk, _, callback) {
       meterBytes.add(chunk.length, context);
+      callbackBytes(chunk.length);
       callback(null, chunk);
     },
   });
