5cf20637b6c5f60ff42f6c2f74999ece05938057
izzy <me@insrt.uk>
2026-02-20 16:01:07 +0000

    chore: update tests & test env.

--- packages/yucca-api/test/auth.e2e-spec.ts
+++ packages/yucca-api/test/auth.e2e-spec.ts
@@ -3,7 +3,6 @@ import { MetricService } from '@common/server/otel';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { parse } from 'cookie';
-import { calculatePKCECodeChallenge, randomPKCECodeVerifier } from 'openid-client';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { controllers, imports, providers } from '../src/app.module';
@@ -79,7 +78,7 @@ describe('AuthController (e2e)', () => {
     });
   });
 
-  describe('GET /auth/oidc', () => {
+  describe('GET /auth/oidc/login', () => {
     it('redirects to IdP', async () => {
       const { header } = await request(app.getHttpServer())
         .get('/api/auth/oidc/login')
@@ -95,9 +94,27 @@ describe('AuthController (e2e)', () => {
 
       expect(header.location).toEqual(expect.stringContaining(env.OIDC_ISSUER.href));
     });
+
+    it('redirects to redirect_uri after IdP', async () => {
+      const { header } = await request(app.getHttpServer())
+        .get('/api/auth/oidc/login?redirect_uri=http://example.com')
+        .set('Cookie', `access-token=${session.accessToken}`)
+        .expect(302);
+
+      const redirectUrl = new URL(header.location);
+      redirectUrl.pathname = '/api/form';
+      redirectUrl.searchParams.set('sub', 'bar');
+
+      const { headers } = await fetch(redirectUrl, {
+        redirect: 'manual',
+      });
+
+      const callbackUrl = headers.get('location')!;
+      expect(callbackUrl).toEqual(expect.stringContaining('http://example.com'));
+    });
   });
 
-  describe('GET /auth/callback', () => {
+  describe('GET /auth/oidc/callback', () => {
     it('should create user if not exists', async () => {
       await expect(testUtils.getUserBySub('bar')).resolves.toBeUndefined();
 
@@ -150,120 +167,6 @@ describe('AuthController (e2e)', () => {
           id: user.id,
         }),
       );
-    });
-  });
-
-  describe('GET /auth/app/login', () => {
-    it('redirects to IdP if logged out', async () => {
-      const { header } = await request(app.getHttpServer())
-        .get('/api/auth/app/login?code_challenge=my-pkce-challenge')
-        .expect(302);
-
-      expect(header['set-cookie']).toEqual(
-        expect.arrayContaining([
-          expect.stringContaining('app-code-challenge=my-pkce-challenge'),
-          expect.stringContaining('oidc-login-flow=app'),
-        ]),
-      );
-
-      expect(header.location).toEqual('/api/auth/oidc/login');
-    });
-
-    it('redirects to app if logged in', async () => {
-      const { header } = await request(app.getHttpServer())
-        .get('/api/auth/app/login?code_challenge=my-pkce-challenge')
-        .set('Cookie', `access-token=${session.accessToken}`)
-        .expect(302);
-
-      expect(header['set-cookie']).toEqual(
-        expect.arrayContaining([expect.stringContaining('app-code-challenge=my-pkce-challenge')]),
-      );
-
-      expect(header.location).toEqual('/login/grant');
-    });
-  });
-
-  describe('POST /auth/app/callback', () => {
-    it('redirects to app', async () => {
-      const { header: loginHeader } = await request(app.getHttpServer())
-        .get('/api/auth/app/login?code_challenge=my-pkce-challenge')
-        .set('Cookie', `access-token=${session.accessToken}`)
-        .expect(302);
-
-      const { header } = await request(app.getHttpServer())
-        .post('/api/auth/app/callback')
-        .set('Cookie', `access-token=${session.accessToken}; ${loginHeader['set-cookie']}`)
-        .expect(302);
-
-      expect(header.location).toEqual(expect.stringContaining('http://localhost:22676/api/auth/callback?code='));
-    });
-  });
-
-  describe('POST /auth/app/token', () => {
-    it('generates a new access token', async () => {
-      const codeVerifier = randomPKCECodeVerifier();
-      const codeChallenge = await calculatePKCECodeChallenge(codeVerifier);
-
-      const { header: loginHeader } = await request(app.getHttpServer())
-        .get(`/api/auth/app/login?code_challenge=${codeChallenge}`)
-        .set('Cookie', `access-token=${session.accessToken}`)
-        .expect(302);
-
-      const { header: callbackHeader } = await request(app.getHttpServer())
-        .post('/api/auth/app/callback')
-        .set('Cookie', `access-token=${session.accessToken}; ${loginHeader['set-cookie']}`)
-        .expect(302);
-
-      const url = new URL(callbackHeader.location);
-
-      const { body } = await request(app.getHttpServer())
-        .post('/api/auth/app/token')
-        .send({
-          codeVerifier,
-          code: url.searchParams.get('code'),
-        })
-        .expect(201);
-
-      expect(body).toEqual(
-        expect.objectContaining({
-          accessToken: expect.any(String),
-        }),
-      );
-    });
-
-    it('fails PKCE if wrong verifier', async () => {
-      const codeVerifier = randomPKCECodeVerifier();
-      const codeChallenge = await calculatePKCECodeChallenge(codeVerifier);
-
-      const { header: loginHeader } = await request(app.getHttpServer())
-        .get(`/api/auth/app/login?code_challenge=${codeChallenge}`)
-        .set('Cookie', `access-token=${session.accessToken}`)
-        .expect(302);
-
-      const { header: callbackHeader } = await request(app.getHttpServer())
-        .post('/api/auth/app/callback')
-        .set('Cookie', `access-token=${session.accessToken}; ${loginHeader['set-cookie']}`)
-        .expect(302);
-
-      const url = new URL(callbackHeader.location);
-
-      await request(app.getHttpServer())
-        .post('/api/auth/app/token')
-        .send({
-          codeVerifier: 'BAD TOKEN',
-          code: url.searchParams.get('code'),
-        })
-        .expect(400);
-    });
-
-    it('fails if missing code', async () => {
-      await request(app.getHttpServer())
-        .post('/api/auth/app/token')
-        .send({
-          codeVerifier: 'BAD TOKEN',
-          code: 'BAD CODE',
-        })
-        .expect(500);
     });
   });
 });
