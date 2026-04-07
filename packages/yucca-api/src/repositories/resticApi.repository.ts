6563f06542f59bc45509c1929d6a64c463b2d3e9
izzy <me@insrt.uk>
2026-04-07 12:19:18 +0100

    fix: don't hardcode the restic api host

--- packages/yucca-api/src/repositories/resticApi.repository.ts
+++ packages/yucca-api/src/repositories/resticApi.repository.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ResticApiRepository {
   getEndpoint() {
-    return new URL(`http://100.64.0.6:${env.RESTIC_API_PORT}`);
-    // return new URL(`http://localhost:${env.RESTIC_API_PORT}`);
+    return new URL(`http://${env.RESTIC_API_HOST}:${env.RESTIC_API_PORT}`);
   }
 }
