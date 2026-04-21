98d5865bc5f457128a499f4c2c8378f08b5b4b2d
izzy <me@insrt.uk>
2026-04-21 09:50:17 +0100

    refactor: split filesystem controller into service

--- packages/yucca-sdk/orchestration-api/src/controllers/filesystem.controller.ts
+++ packages/yucca-sdk/orchestration-api/src/controllers/filesystem.controller.ts
@@ -1,33 +1,16 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOkResponse, ApiQuery } from '@nestjs/swagger';
-import { homedir } from 'node:os';
-import { dirname, resolve } from 'node:path';
 import { FilesystemListingRequestDto, FilesystemListingResponseDto } from '../dto/filesystem.dto';
-import { StorageRepository } from '../repositories/storage.repository';
+import { FilesystemService } from '../services/filesystem.service';
 
 @Controller('/yucca/fs')
 export class FilesystemController {
-  constructor(private readonly storage: StorageRepository) {}
+  constructor(private readonly service: FilesystemService) {}
 
   @Get()
   @ApiQuery({ name: 'path', type: String, required: false })
   @ApiOkResponse({ type: FilesystemListingResponseDto })
   async getFileListing(@Query() dto: FilesystemListingRequestDto): Promise<FilesystemListingResponseDto> {
-    const path = dto.path ?? homedir();
-    const files = await this.storage.readdir(path);
-
-    return {
-      parent: dirname(path),
-      path,
-      items: await Promise.all(
-        files
-          .map((file) => resolve(path, file))
-          .map(async (path) => ({
-            path,
-            isDirectory: await this.storage.stat(path).then((s) => s.isDirectory()),
-          }))
-          .map((result) => result.catch(() => void 0)),
-      ).then((result) => result.filter((entry) => entry !== undefined)),
-    };
+    return this.service.getFileListing(dto);
   }
 }
