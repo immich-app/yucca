35f023b9cfe0d55ba7a2c1b9dbcb25f55582ef90
izzy <me@insrt.uk>
2026-06-11 17:41:14 +0100

    chore: format

--- packages/yucca-api/src/schema/migrations/20260604120000-AddPolarColumnsToUsers.ts
+++ packages/yucca-api/src/schema/migrations/20260604120000-AddPolarColumnsToUsers.ts
@@ -3,10 +3,16 @@ import { Kysely, sql } from 'kysely';
 export async function up(db: Kysely<any>): Promise<void> {
   await sql`ALTER TABLE "users" ADD "polarUserId" character varying; -- missing in target`.execute(db);
   await sql`ALTER TABLE "users" ADD "polarSubscriptionId" character varying; -- missing in target`.execute(db);
-  await sql`ALTER TABLE "users" ADD CONSTRAINT "users_polarUserId_uq" UNIQUE ("polarUserId"); -- missing in target`.execute(db);
-  await sql`ALTER TABLE "users" ADD CONSTRAINT "users_polarSubscriptionId_uq" UNIQUE ("polarSubscriptionId"); -- missing in target`.execute(db);
+  await sql`ALTER TABLE "users" ADD CONSTRAINT "users_polarUserId_uq" UNIQUE ("polarUserId"); -- missing in target`.execute(
+    db,
+  );
+  await sql`ALTER TABLE "users" ADD CONSTRAINT "users_polarSubscriptionId_uq" UNIQUE ("polarSubscriptionId"); -- missing in target`.execute(
+    db,
+  );
   await sql`CREATE INDEX "users_polarUserId_idx" ON "users" ("polarUserId"); -- missing in target`.execute(db);
-  await sql`CREATE INDEX "users_polarSubscriptionId_idx" ON "users" ("polarSubscriptionId"); -- missing in target`.execute(db);
+  await sql`CREATE INDEX "users_polarSubscriptionId_idx" ON "users" ("polarSubscriptionId"); -- missing in target`.execute(
+    db,
+  );
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
