1d94bebcdcff24a11d7605b07a90f9102dd3451b
Paul Makles <me@insrt.uk>
2026-06-25 13:46:23 +0100

    fix: missing GatewayEvent export (#162)

--- packages/yucca-sdk/orchestration-api/src/index.ts
+++ packages/yucca-sdk/orchestration-api/src/index.ts
@@ -1,4 +1,4 @@
 export * from './const';
-export { EventsGateway } from './events/events.gateway';
+export { EventsGateway, type GatewayEvent } from './events/events.gateway';
 export * from './orchestrationApi.module';
 export { ModuleConfigRepository } from './repositories/moduleConfig.repository';
