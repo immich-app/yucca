480d06e0d0b38aae8940d1938b9f3a117405e535
izzy <me@insrt.uk>
2026-09-01 11:21:11 +0100

    fix: set defaults on tables

--- packages/yucca-api/src/schema/migrations/20260901110000-AlignMigrationsToSchema.ts
+++ packages/yucca-api/src/schema/migrations/20260901110000-AlignMigrationsToSchema.ts
@@ -1,19 +1,9 @@
 import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await sql`ALTER TABLE "repositories" ALTER COLUMN "siteCode" SET DEFAULT NULL;`.execute(db);
-  await sql`ALTER TABLE "repositories" ALTER COLUMN "storageClusterCode" SET DEFAULT NULL;`.execute(db);
-  await sql`ALTER TABLE "repositoryMeter" ALTER COLUMN "storageClusterCode" SET DEFAULT NULL;`.execute(db);
-  await sql`ALTER TABLE "repositoryMeterHistory" ALTER COLUMN "storageClusterCode" SET DEFAULT NULL;`.execute(db);
   await sql`CREATE INDEX "userFeatureFlagOverride_userId_idx" ON "userFeatureFlagOverride" ("userId");`.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
   await sql`DROP INDEX "userFeatureFlagOverride_userId_idx";`.execute(db);
-  await sql`ALTER TABLE "repositoryMeter" ALTER COLUMN "storageClusterCode" SET DEFAULT 'local-dev'::text;`.execute(db);
-  await sql`ALTER TABLE "repositoryMeterHistory" ALTER COLUMN "storageClusterCode" SET DEFAULT 'local-dev'::text;`.execute(
-    db,
-  );
-  await sql`ALTER TABLE "repositories" ALTER COLUMN "siteCode" SET DEFAULT 'local'::text;`.execute(db);
-  await sql`ALTER TABLE "repositories" ALTER COLUMN "storageClusterCode" SET DEFAULT 'local-dev'::text;`.execute(db);
 }
