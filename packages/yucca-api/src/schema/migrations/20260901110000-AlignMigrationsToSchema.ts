a812784b6c6425faf0d302f432b1acb40324b212
izzy <me@insrt.uk>
2026-09-01 11:09:47 +0100

    chore: move migration

--- packages/yucca-api/src/schema/migrations/20260901110000-AlignMigrationsToSchema.ts
+++ packages/yucca-api/src/schema/migrations/20260901110000-AlignMigrationsToSchema.ts
@@ -11,7 +11,9 @@ export async function up(db: Kysely<any>): Promise<void> {
 export async function down(db: Kysely<any>): Promise<void> {
   await sql`DROP INDEX "userFeatureFlagOverride_userId_idx";`.execute(db);
   await sql`ALTER TABLE "repositoryMeter" ALTER COLUMN "storageClusterCode" SET DEFAULT 'local-dev'::text;`.execute(db);
-  await sql`ALTER TABLE "repositoryMeterHistory" ALTER COLUMN "storageClusterCode" SET DEFAULT 'local-dev'::text;`.execute(db);
+  await sql`ALTER TABLE "repositoryMeterHistory" ALTER COLUMN "storageClusterCode" SET DEFAULT 'local-dev'::text;`.execute(
+    db,
+  );
   await sql`ALTER TABLE "repositories" ALTER COLUMN "siteCode" SET DEFAULT 'local'::text;`.execute(db);
   await sql`ALTER TABLE "repositories" ALTER COLUMN "storageClusterCode" SET DEFAULT 'local-dev'::text;`.execute(db);
 }
