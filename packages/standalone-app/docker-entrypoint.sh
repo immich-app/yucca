4eb6947e6a267aea06a90620f4ade0a519fdee50
izzy <me@insrt.uk>
2026-09-18 09:49:06 +0100

    fix: ensure data directory exists

--- packages/standalone-app/docker-entrypoint.sh
+++ packages/standalone-app/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 if [ "$(id -u)" = 0 ]; then
+  mkdir -p "${YUCCA_STATE_PATH:-/data}" "${YUCCA_CACHE_PATH:-/cache}"
   chown node:node "${YUCCA_STATE_PATH:-/data}" "${YUCCA_CACHE_PATH:-/cache}"
   exec su-exec node "$@"
 fi
